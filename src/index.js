8de3e30e5083b811ca9d61993746fa0e8bb0ddb2
yuvrajsharmaaa <yuvrajsharmaa2022@gmail.com>
2025-11-23 23:42:54 +0530

    feat: Add buffer and process dependencies to package.json and package-lock.json

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+// Polyfills for Node.js globals in browser
+import { Buffer } from 'buffer';
+window.Buffer = Buffer;
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
